4e5458898a19a4a6c600cd933b32de0c9bfd5722
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-02-13 11:50:05 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,5 +1,10 @@
 # create user 
 class User:
   def __init__(self, user_name, password, email, salt):
+    self.user_name = user_name
+    self.password = password
+    self.eemil = email
+    self.salt = salt
+  
     
 
