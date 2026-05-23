ed17439b45becd848f329977d08653e9002ae649
M Juma <qasimimohammad819@gmail.com>
2026-05-24 00:55:12 +0200

    backend: create account model and use 'AbstractUser'

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,6 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class Account(models.Model):
-    pass
+class Account(AbstractUser):
+    USER_ROLE = [
+        ('admin','Admin'),
+        ('viewer','Viewer'),
+        ('company','Company'),
+        ('job_search','Looking for a job'),
+
+    ]
+    avatar = models.ImageField(upload_to='avatars/',null=True,blank=True)
+    role = models.CharField(max_length=20,choices=USER_ROLE,default='job_search')
+    bio = models.TextField(null=True, blank=True)
+    field_of_stady = models.CharField(max_length=255,null=True,blank=True)
+    skills = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return self.first_name
